a7357562ea4eb306dd8ead042aa8fb54febbeb19
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-09-09 17:03:49 +0000

    Bump:

--- door.py
+++ door.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 import serial
+import os
 
+os.system("./rpi-serial-console enable 9600")
 port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 
-
+def getTag():
+  tag = port.read(14)
+  if tag[0] == 
 
