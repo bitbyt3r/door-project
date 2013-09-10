6b1dca11110763bdc28639b5f51a911b6925deb9
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-09-10 07:22:36 +0000

    First working version of door code

--- door.py
+++ door.py
@@ -2,10 +2,32 @@
 import serial
 import os
 
-os.system("./rpi-serial-console enable 9600")
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
+#os.system("./rpi-serial-console enable 9600")
+port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=30.0)
 
 def getTag():
-  tag = port.read(14)
-  if tag[0] == 
+  while True:
+    port.flushInput()
+    data = [ord(x) for x in port.readline(14)]
+    return data
+
+def rotate(l,n):
+    return l[n:] + l[:n]
+
+def checkTag(tag):
+  for i in xrange(len(tag)):
+    tag = rotate(tag, 1)
+    if len(tag) != 14:
+      continue
+    if tag[0] != 2 and tag[-1] != 3:
+      continue
+    return tag
+  return False
+
+while True:
+  tag = getTag()
+  tag = checkTag(tag)
+  if tag:
+    print "Found Valid Tag:", tag
+
 
