14d4e5872bd3f0f6aa175c97decfb3539ac7b92c
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-09-10 07:42:42 +0000

    Adding motor code part 1

--- door.py
+++ door.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import serial
 import os
+import RPi.GPIO as IO
 
 #os.system("./rpi-serial-console enable 9600")
 port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=30.0)
@@ -30,4 +31,38 @@ while True:
   if tag:
     print "Found Valid Tag:", tag
 
+class Motor:
+  def __init__(self, pins, mode = "Normal", step = 0, resolution = 100, invert=False):
+    self.pins = pins
+    self.mode = mode
+    self.step = 0
+    self.resolution = 100
+    if invert:
+      self.low = IO.HIGH
+      self.high = IO.LOW
+    else:
+      self.low = IO.LOW
+      self.high = IO.HIGH
+    IO.setmode(IO.BCM)
+    for i in self.pins:
+      IO.setup(i, IO.OUT, initial=IO.HIGH)
 
+  def rotate(self, degrees):
+    numSteps = int((degrees / 360) * self.resolution)
+    if numSteps > 0:
+      dir = "Forward"
+    else:
+      dir = "Backward"
+      numSteps = numSteps * -1
+    for i in xrange(numSteps):
+      self.step(dir)
+
+  def step(self, dir):
+    self.pins[self.step], self.low)
+    if dir == "Forward":
+      self.step += 1
+    if dir == "Backward":
+      self.step += -1
+    self.pins[self.step], self.high)
+    
+    
